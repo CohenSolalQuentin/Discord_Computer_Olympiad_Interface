10fce78e304e6a446f50477cafb7dbc75dd675f8
CohenSolalQuentin <144447651+CohenSolalQuentin@users.noreply.github.com>
2025-10-20 23:56:06 +0200

    Update terminal.py

--- discord_interface/utils/terminal.py
+++ discord_interface/utils/terminal.py
@@ -57,6 +57,44 @@ def gris(*txt):
     txt = textify(txt)
     return '\033[0;37m'+txt+'\033[0m'
 
+
+
+def traitement_pourcentage(n, retrait_debut=0, precision=2, taille = 6):
+    demi_taille = taille // 2
+    txt = str(int(round(n*100*10**precision))/(10**precision))+'%'
+    txt = txt.replace('.0%','%')
+    if '.' in txt:
+        d, f = txt.split('.')
+        d = len(d)
+        f = len(f)
+    else:
+        d = len(txt)
+        f = -1
+    full_texte = ' '*(demi_taille-d-retrait_debut)+txt+' '*(demi_taille+(taille % 2)-f)
+    if len(full_texte) == taille:
+        return full_texte
+    elif len(full_texte) == taille+1:
+        if full_texte[0] == ' ':
+            return full_texte[1:]
+        else:
+            if full_texte[-1] == ' ':
+                return full_texte[:-1]
+            else:
+                return full_texte
+
+    elif len(full_texte) == taille+2:
+        if full_texte[0] == ' ' and full_texte[-1] == ' ':
+            return full_texte[1:-1]
+        else:
+            return full_texte
+    elif len(full_texte) > taille:
+        return full_texte
+    else:
+        print(full_texte, len(full_texte), taille)
+        raise Exception('trautement pourcentage ???')
+
+
+
 """Black        0;30     Dark Gray     1;30
 Red          0;31     Light Red     1;31
 Green        0;32     Light Green   1;32
