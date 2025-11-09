f837da78e73d327a9659dca1c9f926bc4eba879d
CohenSolalQuentin <144447651+CohenSolalQuentin@users.noreply.github.com>
2025-11-10 00:17:43 +0100

    Delete translator directory

--- translator/quentin_games/Dames_bresiliennes.py
+++
@@ -1,7 +0,0 @@
-from discord_interface.games.translator.quentin_games.Dames import Draughts
-
-
-class BrazilianDraughts(Draughts):
-    def __init__(self):
-
-        super().__init__(taille=8)
--- translator/quentin_games/Dames_canadiennes.py
+++
@@ -1,8 +0,0 @@
-from discord_interface.games.translator.quentin_games.Dames import Draughts
-
-
-
-class CanadianDraughts(Draughts):
-    def __init__(self):
-
-        super().__init__(taille=12)
