101b52a5112be4853f27635a83b69c973eda72c0
CohenSolalQuentin <144447651+CohenSolalQuentin@users.noreply.github.com>
2025-11-10 21:27:22 +0100

    Delete discord_interface/instances directory

--- discord_interface/instances/connect6.py
+++
@@ -1,9 +0,0 @@
-from discord_interface.games.translator.quentin_games.Connect6 import Connect6
-from discord_interface.games.translator.quentin_games_interface import InterfaceJeuDiscord
-
-
-class Connect6Discord(InterfaceJeuDiscord):
-
-    def __init__(self):
-        super().__init__('connect6', Connect6, params=None, rules='standard', move_keywords=[])
-
